f0b2ac9dccc0dac177a7fbe78b8e4b5e7757e73f
Merghad, Atif <atif.merghad@ibm.com>
2022-09-27 18:04:43 +0100

    add changes to develop

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     version= '0.0.2'
 
     print("Hello world")
-    return {
+    return {  
         'statusCode': 200,
         'body': 'Lambda deployed using github actions',
         'headers': {
