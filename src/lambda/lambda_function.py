a9dc1ea270465ebff18cd25857cc8136d9b0c835
Merghad, Atif <atif.merghad@ibm.com>
2022-11-27 00:02:18 +0100

    update pipeline suport multiple env

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     print("Hello world")
     return {  
         'statusCode': 200,
-        'body': 'Lambda deployed using github  actions 1.3',
+        'body': 'Lambda deployed using github  actions 1.4',
         'headers': {
             "Content-Type": "application/json"
         }    
--- src/python/src/tests/test_normalizer.py
+++ src/python/src/tests/test_normalizer.py
@@ -5,9 +5,6 @@ class MyTestCase(unittest.TestCase):
     def test_good_items(self):
         # Check Kpi
         self.assertEqual("ABC", "ABC")
-    def test_bad_items(self):
-        # Check Kpi
-        self.assertEqual("ABC", "CDF")
         
 
 if __name__ == '__main__':
