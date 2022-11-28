e1862003ffc8b1d34f724048311ce48d0d6eaad4
Merghad, Atif <atif.merghad@ibm.com>
2022-11-28 21:36:23 +0100

    update file

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     print("Hello world")
     return {  
         'statusCode': 200,
-        'body': 'Lambda deployed using github  actions 1.56',
+        'body': 'Lambda deployed using github  actions 1.66',
         'headers': {
             "Content-Type": "application/json"
         }    
